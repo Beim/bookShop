9c8c1bad641c7f221eb2483c706bb329cab579e3
Beim <beim2015@outlook.com>
2016-07-05 20:20:08 +0800

    Added server.log

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ var path = require('path')
 var session = require('koa-session')
 var koaStatic = require('koa-static')
 var db = require('./mongo.js')
-// var views = require('koa-views')
-// var onerror = require('koa-onerror')
-// var fs = require('fs')
+    // var views = require('koa-views')
+    // var onerror = require('koa-onerror')
+var fs = require('fs')
 
 /* routes*/
 var index = require('./routes/index')
@@ -20,21 +20,39 @@ var apiv0 = require('./routes/apiv0')
 	middleware
 */
 app.keys = ['scret', 'keys'] //	key of session
-const opts = {'maxAge': 60 * 60 * 1000}//	maxAge of session
+const opts = {
+        'maxAge': 60 * 60 * 1000
+    } //	maxAge of session
 app.use(session(app, opts))
 app.use(parser({
-	// detectJSON: function (ctx) {
-	//     return /\.json$/i.test(ctx.path);
-	// },
-	extendTypes: {
-	    json: ['application/x-javascript'] // will parse application/x-javascript type body as a JSON string
-	  }
+    // detectJSON: function (ctx) {
+    //     return /\.json$/i.test(ctx.path);
+    // },
+    extendTypes: {
+        json: ['application/x-javascript'] // will parse application/x-javascript type body as a JSON string
+    }
 }))
 app.use(json())
-// app.use(logger())
+    // app.use(logger())
 
-app.use(function * (next) {
-  // var start = new Date()
+app.use(function*(next) {
+    let header = JSON.stringify(this.header)
+    let ws = fs.createWriteStream('./server.log', {
+        flag: 'w+'
+    })
+    let str = `
+        date: ${new Date()}
+        ip: ${this.ip}
+        host: ${this.host}
+        method: ${this.method}
+        header: ${header}
+    `
+    ws.write(str)
+    yield next
+})
+
+app.use(function*(next) {
+    // var start = new Date()
 
     this.set('Access-Control-Allow-Origin', '*')
     this.set('Access-Control-Allow-Methods', 'GET,POST')
@@ -44,8 +62,8 @@ app.use(function * (next) {
         this.status = 200
     }
     yield next
-  // var ms = new Date() - start
-  // console.log('%s%s - %s', this.method, this.url, ms)
+    // var ms = new Date() - start
+    // console.log('%s%s - %s', this.method, this.url, ms)
 })
 
 app.use(koaStatic(path.join(__dirname, '/public')))
